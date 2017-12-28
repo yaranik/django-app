409e9d15a90225c57d54d7220dfdbfce125963e8
yara_nik <yara_nik@mail.ru>
2017-12-28 21:53:36 +0300

    Commenti priehali

--- blog/forms.py
+++ blog/forms.py
@@ -1,1 +1,1 @@
-from django import formsfrom .models import Postclass PostForm(forms.ModelForm):    class Meta:        model = Post        fields = ('title', 'text',)
+from django import formsfrom .models import Post, Commentclass PostForm(forms.ModelForm):    class Meta:        model = Post        fields = ('title', 'text',)class CommentForm(forms.ModelForm):    class Meta:        model = Comment        fields = ('author', 'text',)
